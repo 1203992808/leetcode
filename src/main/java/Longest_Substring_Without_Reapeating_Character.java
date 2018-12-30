b63ca07cd88c958fdabdb110eb0523fc497baafe
1203992808 <1203992808@qq.com>
2018-12-30 18:44:55 +0800

    leetcode 4 无重复字符的最长子串

--- src/main/java/Longest_Substring_Without_Reapeating_Character.java
+++ src/main/java/Longest_Substring_Without_Reapeating_Character.java
@@ -2,10 +2,14 @@
  * @author syz
  * @date 2018-12-19 20:19
  */
+
+import java.util.HashMap;
+import java.util.Map;
+
 /***leetcode 4 无重复字符的最长子串
  * **/
 public class Longest_Substring_Without_Reapeating_Character {
-    public static  int   f1 (String str) {
+    public static  int   f1 (String str) { //用数组实现哈希表
         if (str == null || str.length() == 0) {
             return 0;
         }
@@ -35,9 +39,36 @@ public class Longest_Substring_Without_Reapeating_Character {
         }
         return result;
     }
+    public static int f2 (String str) {//用java自带的map
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        Map<Character,Integer> map = new HashMap<>();
+        int slow = 0;
+        int fast = 0;
+        int max = 0;
+        for ( fast = 0; fast <str.length() ; fast++) {
+            char currentChar = str.charAt(fast);
+            if (map.get(currentChar) == null) {
+                map.put(currentChar,fast);
+                max = Math.max(max,fast - slow +1);
+            } else {
+                int lastPosition = map.get(currentChar);
+                if (lastPosition < slow) {
+                    map.put(currentChar,fast);
+                    max = Math.max(max,fast - slow +1);
+                } else {
+                    slow = lastPosition +1;
+                    map.put(currentChar,fast);
+                }
+            }
+        }
+        return max;
+    }
     public static void main(String[] args) {
         String str = "abcabcbb";
         System.out.println(f1(str));
+        System.out.println(f2(str));
     }
 
 }
