6d25e6179f102e4b2b50b9f58247d3021d1c689f
1203992808 <1203992808@qq.com>
2019-01-01 14:53:07 +0800

    eetcode 11 盛最多水的容器

---
+++ src/main/java/Container_With_Most_Water.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+
+/**
+ * leetcode 11 盛最多水的容器
+ * @author syz
+ * @date 2019-01-01 12:34
+ */
+public class Container_With_Most_Water {
+    public static int f1 (int[] arr) {
+        int leftMax = arr[0];
+        int rightMax = arr[arr.length - 1];
+        int left = 0;
+        int right = arr.length - 1;
+        int max = 0;
+        while (left < right) {
+            if (arr[left] < leftMax) {
+                left++;
+                continue;
+            }
+            if (arr[right]< rightMax) {
+                right--;
+                continue;
+            }
+            leftMax = arr[left];
+            rightMax = arr[right];
+            max = Math.max(Math.min(leftMax,rightMax)*(right - left),max);
+            if (leftMax > rightMax) {
+                right--;
+            }else {
+                left++;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,8,6,2,5,4,8,3,7};
+        System.out.println(f1(arr));
+    }
+}
